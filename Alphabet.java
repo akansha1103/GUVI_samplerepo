f9b19799b1f8404cef0135e931316f7b617e9aee
Akansha Khandelwal <33524795+akansha1103@users.noreply.github.com>
2018-11-30 19:00:48 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Alphabet
+{
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+        char ch = s.next().charAt(0);
+        if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z')
+        {
+            System.out.println("Alphabet");
+        }
+        
+        else
+        System.out.println("No");
+	}
+}
