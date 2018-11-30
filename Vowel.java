1c4e12a6ffe3efe9dd9d982578fe348e9e1e986e
Akansha Khandelwal <33524795+akansha1103@users.noreply.github.com>
2018-11-30 18:53:30 +0530

    Vowel or consonant

---
+++ Vowel.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Vowel
+{
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+        char ch = s.next().charAt(0);
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+        {
+            System.out.println("Vowel");
+        }
+        else if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z')
+        System.out.println("Consonant");
+        else
+        System.out.println("invalid");
+	}
+}
