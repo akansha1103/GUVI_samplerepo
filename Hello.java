4578e6e506801b7f68b78dbab7306e852dcc8f4f
Akansha Khandelwal <33524795+akansha1103@users.noreply.github.com>
2018-11-30 19:43:35 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Hello
+{
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n==0)
+        {
+            System.out.println("");
+        }
+        while(n!=0){
+        System.out.println("Hello");
+        n--;
+	}
+	}
+}
